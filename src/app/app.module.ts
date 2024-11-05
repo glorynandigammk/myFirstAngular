b8ed7089e0e0d96b08b36e175571248e2501ebeb
glory <glorynandigam@gmail.com>
2024-11-05 15:59:23 +0000

    updated modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms'
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { TaskComponent } from './tasks/task/task.component';
+import { CardComponent } from './shared/card/card.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  bootstrap: [AppComponent],
-  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
+  bootstrap:[AppComponent],
+  declarations: [AppComponent,HeaderComponent, UserComponent, TasksComponent,
+    TaskComponent,CardComponent,NewTaskComponent],
+  imports: [BrowserModule,FormsModule],
 })
 export class AppModule {}
